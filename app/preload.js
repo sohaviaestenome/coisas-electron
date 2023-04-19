f979783b0210763f55b8710b8d20c0b67ec41468
Pedro Silva <pacs@outlook.pt>
2023-04-19 17:58:35 +0100

    added preload.js for exposing electron functions in react

---
+++ app/preload.js
@@ -0,0 +1,17 @@
+//preload.js
+const { contextBridge, ipcRenderer } = require('electron');
+
+console.log('Preload script loaded');
+
+contextBridge.exposeInMainWorld('electron', {
+  send: (channel, data) => {
+    ipcRenderer.send(channel, data);
+    console.log(channel, 'preload channel');
+  },
+  receive: (channel, callback) => {
+    ipcRenderer.on(channel, (event, ...args) => callback(...args));
+  },
+  invoke: (channel, ...args) => {
+    return ipcRenderer.invoke(channel, ...args);
+  },
+});
