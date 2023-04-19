33a49effe6e03087c43d6787545a15803391311c
Pedro Silva <pacs@outlook.pt>
2023-04-19 16:50:37 +0100

    installed more dependencies, created electron folder, created main windows

---
+++ app/electron.js
@@ -0,0 +1,49 @@
+//main.js
+const { app, BrowserWindow, ipcMain, dialog } = require("electron");
+const path = require("path");
+const dotenv = require("dotenv");
+const { moveFilesToParentFolder } = require('./movefiles');
+
+dotenv.config();
+
+console.log('Preload path:', path.join(__dirname, 'preload.js'));
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js'),
+    }
+  });
+
+  const url =
+    process.env.ELECTRON_ENV === "development"
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`;
+
+  win.loadURL(url);
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
